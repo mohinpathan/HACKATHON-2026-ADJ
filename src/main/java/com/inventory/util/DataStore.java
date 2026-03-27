48b3a43a730662eeecc1fa476d7a9f424968369b
mohinkhan1118 <89690048+mohinpathan@users.noreply.github.com>
2026-03-27 12:36:53 +0530

    add dummy data temp data

---
+++ src/main/java/com/inventory/util/DataStore.java
@@ -0,0 +1,21 @@
+package com.inventory.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.inventory.model.Product;
+
+public class DataStore {
+
+    public static List<Product> products = new ArrayList<>();
+
+    // STATIC BLOCK = runs automatically when class loads
+    static {
+        products.add(new Product(1, "Fast Charger", "Charger", 20, 499));
+        products.add(new Product(2, "Type-C Cable", "Cable", 5, 199));
+        products.add(new Product(3, "Wireless Earbuds", "Audio", 8, 1499));
+        products.add(new Product(4, "Mobile Cover", "Cover", 15, 299));
+        products.add(new Product(5, "Power Bank", "Battery", 3, 999));
+        products.add(new Product(6, "Screen Guard", "Protection", 25, 149));
+    }
+}
